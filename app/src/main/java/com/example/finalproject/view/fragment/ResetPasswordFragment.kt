8c8fdde13d8deece710c9c1dafb476b6013718c8
anzila26 <sitianzilanur@gmail.com>
2023-06-05 22:15:42 +0700

    add layout otp and reset pass

---
+++ app/src/main/java/com/example/finalproject/view/fragment/ResetPasswordFragment.kt
@@ -0,0 +1,23 @@
+package com.example.finalproject.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.finalproject.R
+import com.example.finalproject.databinding.FragmentResetPasswordBinding
+import com.example.finalproject.databinding.FragmentSplashBinding
+
+class ResetPasswordFragment : Fragment() {
+    lateinit var binding : FragmentResetPasswordBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentResetPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/finalproject/view/fragment/SplashFragment.kt
@@ -0,0 +1,23 @@
+package com.example.finalproject.view.fragment
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.finalproject.R
+import com.example.finalproject.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+    lateinit var binding : FragmentSplashBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
