c87ef001456aac35c5b73a9141d3abb0f64d4bfb
Fikri Haikal <fikrihaikal571@gmail.com>
2023-06-07 01:03:04 +0700

    menambahkan layout home, dan bottom navigation, fragment history,notif,akun - Rogo

---
+++ app/src/main/java/com/example/finalproject/view/adapter/HomeAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.finalproject.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finalproject.databinding.DestinasiItemBinding
+import com.example.finalproject.model.data.DestinasiItem
+
+class HomeAdapter(private val listItem:List<DestinasiItem>): RecyclerView.Adapter<HomeAdapter.ItemViewHolder>(){
+
+    class ItemViewHolder(private val binding : DestinasiItemBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(item:DestinasiItem){
+            binding.ivDestinasi.setImageResource(item.img)
+            binding.tvDestinasi.text = item.jenisDestinasi
+            binding.tipePesawat.text = item.tipePesawat
+            binding.tvTanggal.text = item.tgl
+            binding.tvMulaiDari.text = item.harga
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = DestinasiItemBinding.inflate(inflater,parent,false)
+        return ItemViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return listItem.size
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = listItem[position]
+        holder.bind(item)
+    }
+}
